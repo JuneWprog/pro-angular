f5a5994578b8f740c3f6b8ba708d8f2c0224b187
Jun wang <wang.jun6@northeastern.edu>
2025-04-18 22:28:15 -0600

    todo list

---
+++ udemy-angular/src/app/user.component.ts
@@ -0,0 +1,62 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, computed, input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  template: `
+    <div>
+      <button  (click) ="onSelectUser()">
+        <!-- using signal -->
+        <!-- <img [src]="imagePath()" [alt]="user().name" width="50" height="50">
+        <span>{{ user().name }}</span> -->
+
+        <!-- old way -->
+        <img [src]="imagePath" [alt]="user.name" width="50" height="50">
+        <span>{{ user.name }}</span>
+      </button>
+    </div>
+  `,
+  styleUrls: ['./user.component.css'], // optional if you want styling
+})
+export class UserComponent {
+    //old way
+  @Input({required:true}) user: any;           // required
+  @Input({required:true}) selectedUserId: string | null = null; // required
+  @Output() select = new EventEmitter<string>(); // output event emitter
+
+
+  get imagePath(){
+    return '/assets/' + this.user.avatar
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id); // emit the selected user id
+  }
+
+
+
+// //using signal ->  now user is a function  user()
+// user = input.required<{ id: string; name: string; avatar: string }>();
+
+// //must use computed to get the image path
+// imagePath = computed(() => {
+//   return '/assets/' + this.user().avatar; 
+// })
+// select = output <string>();
+
+// onSelectUser() {
+//     this.select.emit(this.user.id); // emit the selected user id
+//   }
+
+
+
+
+}
+
+
+
+
+
+
+
